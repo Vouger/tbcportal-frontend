64ae1e0b8ee1beee967c6ced32ccb1fb3137357a
Roman <rvougers@gmail.com>
2021-04-08 14:23:55 +0300

    side menu selected

--- src/modules/layout/components/SideMenuItem/SideMenuItem.jsx
+++ src/modules/layout/components/SideMenuItem/SideMenuItem.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import {ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
+import { useLocation } from "react-router-dom";
+import clsx from "clsx";
+
+import styles from "./SideMenuItem.module.scss";
 
 export default function SideMenuItem (props) {
+    const location = useLocation();
+    const isCurrent = location.pathname === props.to;
+
     return (
-        <ListItem button component={props.component} to={props.to} onClick={props.onClick}>
+        <ListItem
+            button
+            component={props.component}
+            to={props.to}
+            onClick={props.onClick}
+            className={clsx(styles.root, isCurrent && styles.current)}
+        >
             <ListItemIcon>
                 {props.icon && (<props.icon />)}
             </ListItemIcon>
