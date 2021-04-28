e48d12d83a3177fafe49b8669bb655f5f895d1fb
Roman <rvougers@gmail.com>
2021-04-28 19:49:11 +0300

    refactoring

--- src/@queries/guides.js
+++ src/@queries/guides.js
@@ -26,9 +26,9 @@ export default class Guides {
                     views
                     className
                     thumbnailUrl
-                        user {
-                            nickname
-                        }
+                    user {
+                        nickname
+                    }
                 }
                 total
             }
