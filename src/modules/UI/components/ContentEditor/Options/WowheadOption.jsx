7cada3453419c75744501697e5bc874b24530104
Roman <rvougers@gmail.com>
2021-05-12 14:22:45 +0300

    wowhead button

--- src/modules/UI/components/ContentEditor/Options/WowheadOption.jsx
+++ src/modules/UI/components/ContentEditor/Options/WowheadOption.jsx
@@ -2,14 +2,55 @@ import React, {useState} from "react";
 import {EditorState, Modifier} from "draft-js";
 import {Avatar} from "@material-ui/core";
 
-import styles from "modules/UI/components/ContentEditor/Options/WowheadOption.module.scss";
 import PopupOption from "modules/UI/components/ContentEditor/Options/PopupOption";
+import styles from "modules/UI/components/ContentEditor/Options/WowheadOption.module.scss";
 
 function WowheadOption(props) {
     const { editorState, onChange, modalHandler } = props;
     const [buttonDisabled, setButtonDisabled] = useState(true);
+    const [value, setValue] = useState('');
+    const [error, setError] = useState('');
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+
+        if (event.target.value.length !== 0) {
+            setButtonDisabled(false)
+        } else {
+            setButtonDisabled(true)
+        }
+    }
 
     const submitAction = () => {
+        try {
+            new URL(value);
+        } catch (e) {
+            setError('Not valid url');
+
+            return false;
+        }
+
+        const entityKey = editorState
+            .getCurrentContent()
+            .createEntity('LINK', 'MUTABLE', { url: value, targetOption:'_blank' })
+            .getLastCreatedEntityKey();
+
+        let contentState = Modifier.replaceText(
+            editorState.getCurrentContent(),
+            editorState.getSelection(),
+            `wowhead`,
+            editorState.getCurrentInlineStyle(),
+            entityKey
+        );
+        let newEditorState = EditorState.push(
+            editorState,
+            contentState,
+            'insert-characters'
+        );
+
+        onChange(newEditorState);
+        setValue('')
+        setError('');
 
         return true;
     }
@@ -27,6 +68,23 @@ function WowheadOption(props) {
                     <span className="rdw-embedded-modal-header-label"></span>
                 </span>
             </div>
+
+            {
+                error
+                    ? (<span className={styles.error}>{error}</span>)
+                    : ''
+            }
+
+            <span className="rdw-embedded-modal-link-input-wrapper">
+                <input
+                    className="rdw-embedded-modal-link-input"
+                    placeholder="Enter link"
+                    name="embeddedLink"
+                    value={value}
+                    onChange={handleChange}
+                />
+                <span className="rdw-image-mandatory-sign">*</span>
+            </span>
         </PopupOption>
     );
 }
