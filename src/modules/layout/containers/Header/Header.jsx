f8a87c9c20a80cc22a8735ce2f741a82fb6ab755
Roman <rvougers@gmail.com>
2021-06-14 16:34:24 +0300

    header redesign

--- src/modules/layout/containers/Header/Header.jsx
+++ src/modules/layout/containers/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton, Box } from '@material-ui/core'
+import {AppBar, Toolbar, IconButton, Box, Container} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
 import PropTypes from "prop-types";
 
@@ -11,30 +11,32 @@ import styles from './Header.module.scss'
 function Header(props) {
     return (
         <AppBar position="absolute" color="default" className={styles.root}>
-            <Toolbar className={styles.toolbar}>
+            <Container maxWidth="xl">
+                <Toolbar className={styles.toolbar}>
 
-                <Box display={{ xs: 'block', sm: 'none' }}>
-                    <IconButton
-                        edge="start"
-                        color="secondary"
-                        aria-label="open drawer"
-                        onClick={() => { props.setOpen(true) }}
-                        className={styles.menuButton}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                </Box>
+                    <Box display={{ xs: 'block', sm: 'none' }}>
+                        <IconButton
+                            edge="start"
+                            color="secondary"
+                            aria-label="open drawer"
+                            onClick={() => { props.setOpen(true) }}
+                            className={styles.menuButton}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    </Box>
 
-                <Logo title={props.title} />
+                    <Logo title={props.title} />
 
-                <Box className={styles.menu} display={{ xs: 'none', sm: 'block' }}>
-                    <Navigation isAuth={props.isAuth} role={props.role} />
-                </Box>
+                    <Box className={styles.menu} display={{ xs: 'none', sm: 'block' }}>
+                        <Navigation isAuth={props.isAuth} role={props.role} />
+                    </Box>
 
-                <Box display={{ xs: 'none', sm: 'block' }}>
-                    <UserMenu isAuth={props.isAuth} />
-                </Box>
-            </Toolbar>
+                    <Box display={{ xs: 'none', sm: 'block' }}>
+                        <UserMenu isAuth={props.isAuth} />
+                    </Box>
+                </Toolbar>
+            </Container>
         </AppBar>
     )
 }
