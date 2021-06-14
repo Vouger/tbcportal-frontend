f688c02a29b02d8fc3f876e6091dddb771fbdf17
Roman <rvougers@gmail.com>
2021-06-14 16:33:19 +0300

    new logo

--- src/modules/layout/components/Logo/Logo.jsx
+++ src/modules/layout/components/Logo/Logo.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {Avatar, Typography} from "@material-ui/core";
 
 import styles from "./Logo.module.scss";
 
 export default function Logo(props) {
     return (
         <Link to="/" className={styles.root}>
-            <Avatar alt="Vanilla LFG" variant="square" src="/logo.jpg"/>
-            <Typography variant="h6" noWrap className={styles.title}>
-                {props.title || "Vanilla LFG" }
-            </Typography>
+            <img alt={props.title || "Vanilla LFG"} src="/header_logo.png"/>
         </Link>
     )
 }
